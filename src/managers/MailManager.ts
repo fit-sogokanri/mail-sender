45235f7bbf942a31035a2988cc75ad0cfd3a341a
SouTamura <tamuchan0209@gmail.com>
2024-12-16 22:20:28 +0900

    fix: transportの設定値を適切に読み取るように修正

--- src/managers/MailManager.ts
+++ src/managers/MailManager.ts
@@ -1,13 +1,15 @@
 import { createTransport } from 'nodemailer';
+import { config } from "dotenv-safe";
 import Mailer from "nodemailer/lib/mailer";
 import {Mail} from "../models/Mail";
+config();
 
 export class MailManager {
     private static transporter: Mailer = createTransport({
         pool: true,
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: process.env.SMTP_SECURE, // use TLS
+        port: parseInt(process.env.SMTP_PORT),
+        secure: process.env.SMTP_SECUR==="true",
         auth: {
             user: process.env.SMTP_USERNAME,
             pass: process.env.SMTP_PASSWORD,
