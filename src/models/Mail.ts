2284fc01ec402d09dbc163e89a9d5f9ee4100450
SouTamura <tamuchan0209@gmail.com>
2024-12-16 22:10:55 +0900

    feat: 本文の形式とpriority、replyToの指定に対応

--- src/models/Mail.ts
+++ src/models/Mail.ts
@@ -2,45 +2,78 @@ import {MailAddress} from "./MailAddress";
 import {MailAttachment} from "./MailAttachment";
 import Mailer from "nodemailer/lib/mailer";
 
+export const ContentType = {
+    TEXT: "text",
+    HTML: "html",
+} as const;
+export type ContentType = (typeof ContentType)[keyof typeof ContentType];
+
+export const Priority = {
+    HIGH: "high",
+    NORMAL: "normal",
+    LOW: "low",
+} as const;
+export type Priority = (typeof Priority)[keyof typeof Priority];
+
 export class Mail {
     private from: MailAddress;
     private to: MailAddress[];
     private cc: MailAddress[];
     private bcc: MailAddress[];
-
+    private reply_to: MailAddress|undefined;
     private subject: string;
-    private html: string;
+    private content: string;
+    private content_type: ContentType;
     private attachments: MailAttachment[];
+    private priority: Priority|undefined;
 
     constructor(
         from: MailAddress,
         to: MailAddress[],
         subject: string,
-        html: string,
+        content: string,
+        content_type: ContentType,
         cc?: MailAddress[],
         bcc?: MailAddress[],
-        attachments?: []
+        reply_to?: MailAddress,
+        attachments?: MailAttachment[],
+        priority?: Priority,
     ) {
         this.from = from;
         this.to = to;
         this.subject = subject;
-        this.html = html;
+        this.content = content;
+        this.content_type = content_type;
         this.cc = cc?.length?cc:[];
         this.bcc = bcc?.length?bcc:[];
+        this.reply_to = reply_to;
         this.attachments = attachments?.length?attachments:[];
+        this.priority = priority;
     }
 
-    public get_message_object(): Mailer.Options{
-        return {
+    public get_message_object(): Mailer.Options {
+        const body: Mailer.Options = {
             from: this.from.get_address_object(),
-            to: this.to.map(to_v=>{return to_v.get_address_object()}),
+            to: this.to.map(to_v => {
+                return to_v.get_address_object()
+            }),
             subject: this.subject,
-            html: this.html,
-
-            cc: this.cc.map(cc_v=>{return cc_v.get_address_object()}),
-            bcc: this.bcc.map(bcc_v=>{return bcc_v.get_address_object()}),
-            attachments: this.attachments.map((attachment: MailAttachment) => {return attachment.get_object()}),
+            cc: this.cc.map(cc_v => {
+                return cc_v.get_address_object()
+            }),
+            bcc: this.bcc.map(bcc_v => {
+                return bcc_v.get_address_object()
+            }),
+            attachments: this.attachments.map((attachment: MailAttachment) => {
+                return attachment.get_object()
+            }),
         }
-    }
 
+        body[this.content_type] = this.content;
+
+        if(this.reply_to) body["replyTo"] = this.reply_to.get_address();
+        if(this.priority) body["priority"] = this.priority;
+
+        return body
+    }
 }
