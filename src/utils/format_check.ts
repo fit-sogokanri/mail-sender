67d5e8ab0b31baeca5f7f0e8f01bb43141a05f48
SouTamura <tamuchan0209@gmail.com>
2024-12-18 00:58:08 +0900

    feat: メール関係の値のフォーマットチェック関数を追加

--- src/utils/format_check.ts
+++ src/utils/format_check.ts
@@ -1,3 +1,6 @@
+import Mailer from "nodemailer/lib/mailer";
+import {MailAddress} from "../models/MailAddress";
+import {ContentType, Priority} from "../models/Mail";
 
 const uid = (uid_str: string): boolean =>{
     const regex = new RegExp("^[a-z][0-9a-z_\-]{4,63}$", "g")
@@ -9,10 +12,11 @@ const name = (name_str: string): boolean =>{
     return length<256;
 }
 
-const mail = (mail_str: string): boolean =>{
+const mail_address = (address_str: string): boolean =>{
     const regex = new RegExp("^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\\.)+[a-zA-Z]{2,}$", "g")
-    return regex.test(mail_str);
+    return regex.test(address_str);
 }
+
 
 const password = (password_str: string): boolean =>{
     const regex = new RegExp("^[0-9A-Za-z\-\_\/\*\+\.\,\!\#\$\%\&\(\)\~\|]{8,}$", "g")
@@ -29,9 +33,9 @@ const idm = (idm_string: string): boolean =>{
     return regex.test(idm_string);
 }
 
-const note = (note_string: string): boolean =>{
-    const regex = new RegExp(".{0,1024}", "g")
-    return regex.test(note_string);
+const text = (text_string: string): boolean =>{
+    const regex = new RegExp(".{1,}", "g")
+    return regex.test(text_string);
 }
 
 const description = (note_string: string): boolean =>{
@@ -57,7 +61,75 @@ const uuid = (uuid_string: string): boolean=>{
     return true;
 }
 
+const mail_address_object = (address_object: unknown): address_object is Mailer.Address =>{
+    if (typeof address_object !== "object" || address_object === null) {
+        return false;
+    }
+    const { name, address } = address_object as Record<keyof Mailer.Address, unknown>;
 
+    if (typeof name !== "string") return false;
+    if (typeof address !== "string" || !mail_address(address)) return false;
+
+    return true;
+}
+
+const input_mail_address = (address: any): boolean=>{
+    if(typeof address === 'string' && mail_address(address)) return true;
+    if(typeof address === 'object' && mail_address_object(address)) return true;
+
+    return false;
+}
+
+const input_mail_address_array = (address_array: any): boolean=>{
+    if(Array.isArray(address_array)){
+        return address_array.some((address: any) => input_mail_address(address));
+    }
+    return false;
+}
+
+const attachment_object = (attachment: any): boolean => {
+    if (typeof attachment !== "object" || attachment === null) {
+        return false;
+    }
+    const {name, content} = attachment as Record<keyof { name: string, content: string }, unknown>;
+
+    if (typeof name !== "string") return false;
+    if (typeof content !== "string") return false;
+
+    return true;
+}
+
+const attachment_array = (attachment_array: any): boolean=>{
+    if(Array.isArray(attachment_array)){
+        return attachment_array.some((attachment: any) => attachment_object(attachment));
+    }
+    return false;
+}
+
+const content_type = (content_type_string: string): boolean=>{
+    return  content_type_string === ContentType.HTML || content_type_string === ContentType.TEXT;
+}
+
+const priority = (priority_string: string): boolean=>{
+    return  priority_string === Priority.HIGH || priority_string === Priority.NORMAL || priority_string === Priority.LOW;
+}
+
+export const Mail = {
+    mail_address: mail_address,
+    mail_address_object: mail_address_object,
+    from: input_mail_address,
+    to: input_mail_address_array,
+    cc: input_mail_address_array,
+    bcc: input_mail_address_array,
+    reply_to: mail_address,
+    subject: text,
+    content: text,
+    content_type: content_type,
+    attachment: attachment_object,
+    attachments: attachment_array,
+    priority: priority,
+
+}
 
 export const Tokens = {
     uuid: uuid,
