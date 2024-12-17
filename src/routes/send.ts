e4bb57b55b2c2936a74c6b03604c0c52cc4becb5
SouTamura <tamuchan0209@gmail.com>
2024-12-18 01:14:00 +0900

    feat: メール送信機能を実装

--- src/routes/send.ts
+++ src/routes/send.ts
@@ -1,13 +1,27 @@
 import express from "express";
-import {send400} from "../utils/error_response";
+import {send400_with_message, send500_with_message} from "../utils/error_response";
 import {validate_authorization} from "../utils/validate_authorization";
+import {MailManager} from "../managers/MailManager";
+import {Mail} from "../models/Mail";
+import {FormatError} from "../models/errors/FormatError";
+import {SendMailError} from "../models/errors/SendMailError";
 
-const debug = require('debug')('mai-sender:routes:logout')
+const debug = require('debug')('mai-sender:routes:send')
 
 export const router = express.Router();
 
 router.post("/",
     validate_authorization,
     async (req, res) => {
-        res.status(200).json({message: "ok"});
+        const body = await req.body;
+
+        try{
+            const mail = Mail.parse(body);
+            const mail_response = await MailManager.send(mail);
+            res.status(200).send(mail_response.get_object());
+        }catch(e){
+            if(e instanceof FormatError) return send400_with_message(res, `${e.message}: ${e.bad_points.join(", ")}`);
+            if(e instanceof SendMailError) return send500_with_message(res, `${e.message}`);
+            send500_with_message(res, `unknown error.`);
+        }
     });
