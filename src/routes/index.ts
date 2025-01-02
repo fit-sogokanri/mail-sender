caf3f8c58c0014adba14cbea424bfb716dc6f329
SouTamura <tamuchan0209@gmail.com>
2025-01-03 01:12:40 +0900

    change: /index.htmlを受け付けられるように変更

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,6 @@ import {router as send_router} from "./send"
 export const router = express.Router();
 router.use('/send', send_router);
 
-router.get('/', (req, res) => {
+router.get('/(|index.html)', (req, res) => {
     return res.status(200).send({"message": "reached"});
 })
